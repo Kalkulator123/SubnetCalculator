ea5ceca512afc8490f728df700428db2ee94e59f
NBright <71440500+NBright04@users.noreply.github.com>
2021-11-09 20:29:37 +0100

    ui

---
+++ src/main/java/com/kalkulator123/subnetcalculator/MainController.java
@@ -0,0 +1,14 @@
+package com.kalkulator123.subnetcalculator;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    //@FXML
+    //private Label welcomeText;
+
+    @FXML
+    protected void onGithubButtonClick() {
+
+    }
+}
--- src/main/java/com/kalkulator123/subnetcalculator/SubnetCalculator.java
+++ src/main/java/com/kalkulator123/subnetcalculator/SubnetCalculator.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -12,26 +13,14 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class SubnetCalculator extends Application {
-    private double xOffset = 0;
-    private double yOffset = 0;
-
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-view.fxml")));
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
-
-        root.setOnMousePressed((MouseEvent event) -> {
-            xOffset = event.getSceneX();
-            yOffset = event.getSceneY();
-        });
-
-        root.setOnMouseDragged((MouseEvent event) -> {
-            stage.setX(event.getSceneX() - xOffset);
-            stage.setY(event.getSceneY() - yOffset);
-        });
+        stage.setResizable(false);
+        stage.setTitle("Subnet Calculator");
 
         stage.setScene(scene);
         stage.show();
